863db50c27b4251fea3f10f124170dae715b0ab7
Cesar Mora <calfonsomora@gmail.com>
2025-09-24 16:23:37 -0500

    fix: Update environment import to use the correct configuration for the service

--- src/app/pagina-consultor/servicios/servicios-consultor.service.ts
+++ src/app/pagina-consultor/servicios/servicios-consultor.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment.qa';
+import { environment } from 'src/environments/environment';
 import { Autenticable } from 'src/app/administrador/servicios/compartido/Autenticable';
 
 @Injectable({
